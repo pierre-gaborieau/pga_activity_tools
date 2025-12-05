ee9af77729dbc8083608f4afb5bc07c960a90464
pierre-gaborieau <55131762+pierre-gaborieau@users.noreply.github.com>
2025-12-05 09:26:20 +0100

    feat(3): Add wind direction to description (#17)
    * WIP Wind Direction
    
    * Fix typo

--- pgaActivityTools/Models/Weather/WeatherData.cs
+++ pgaActivityTools/Models/Weather/WeatherData.cs
@@ -7,6 +7,7 @@ public class WeatherData
     public double FeelsLike { get; set; }
     public int Humidity { get; set; }
     public double WindSpeed { get; set; }
+    public double WindAngle { get; set; }
     public int Cloudiness { get; set; }
     public string CityName { get; set; } = string.Empty;
     public DateTime Timestamp { get; set; }
